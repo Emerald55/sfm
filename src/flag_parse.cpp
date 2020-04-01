13dddd42af836f016fdeb9e43017c323a615a4e9
Emerald55 <24934884+Emerald55@users.noreply.github.com>
2020-04-01 18:32:34 -0400

    Added long flag options

--- src/flag_parse.cpp
+++ src/flag_parse.cpp
@@ -1,16 +1,23 @@
 #include "flag_parse.h"
+#include <getopt.h>
 #include <unistd.h>
 #include <iostream>
 
 bool parse_flags(bool &show_symbolic_links, bool &show_hidden_files, int argc, char* argv[]) {
+	const option long_opts[] = {
+		{"help", no_argument, nullptr, 'h'},
+		{"all", no_argument, nullptr, 'a'},
+		{"symlinks", no_argument, nullptr, 's'},
+		{"path", required_argument, nullptr, 'p'}
+	};
 	int opt;
-	while((opt = getopt(argc, argv, ":p:has")) != -1) {
+	while((opt = getopt_long(argc, argv, ":p:has", long_opts, nullptr)) != -1) {
 		switch (opt) {
 			case '?':
 			       std::cerr << "Invalid flag, type \"-h\" for options.\n";
 			       return false;
 			case ':':
-			       std::cerr << "A flag is missing an extra value.\n";
+			       std::cerr << "A flag is missing a required argument.\n";
 			       return false;
 			case 'a':
 			       show_hidden_files = true;
@@ -20,10 +27,10 @@ bool parse_flags(bool &show_symbolic_links, bool &show_hidden_files, int argc, c
 			       break;
 			case 'h':
 				std::cout << "Sfm Usage:\n";
-				std::cout << "	-h                Displays help options\n";
-				std::cout << "	-a                Unhides files starting with a \".\"\n";
-				std::cout << "	-s                Shows symlinks\n";
-				std::cout << "	-p /start/path    Path to start program from\n";
+				std::cout << "	-h --help              Displays help options\n";
+				std::cout << "	-a --all               Unhides files starting with a \".\"\n";
+				std::cout << "	-s --symlinks          Shows symlinks\n";
+				std::cout << "	-p --path /start/path  Path to start program from\n";
 				return false;
 			case 'p':
 				errno = chdir(optarg);
