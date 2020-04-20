4097ef77a29dcd5e50b44a9e92a8f6ece0181b5d
Emerald55 <24934884+Emerald55@users.noreply.github.com>
2020-04-20 07:05:47 -0400

    Refactored getting files

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -34,19 +34,16 @@ std::vector<std::string> file_io::get_dir_files(const std::string &path, bool sh
 		const std::string &search_str) {
 	std::vector<std::string> files;
 	for (const auto &entry : std::filesystem::directory_iterator(path)) {
-		bool was_pushed = false;
 		if (show_hidden_files) {
-			files.push_back(entry.path());
-			was_pushed = true;
+			if (path_to_filename(entry.path().string()).find(search_str) !=
+					std::string::npos) {
+				files.push_back(entry.path());
+			}
 		}
 		else if (path_to_filename(entry.path().string()).rfind(".", 0) != 0) {
-			files.push_back(entry.path());
-			was_pushed = true;
-		}
-		if (was_pushed && !search_str.empty()) {
-			if (path_to_filename(entry.path().string()).find(search_str) ==
+			if (path_to_filename(entry.path().string()).find(search_str) !=
 					std::string::npos) {
-				files.pop_back();
+				files.push_back(entry.path());
 			}
 		}
 	}
